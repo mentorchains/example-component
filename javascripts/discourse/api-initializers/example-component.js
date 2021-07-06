095f90de04353bde386a3b75efcab4eea783d368
Keegan George <kgeorge13@gmail.com>
2021-07-06 15:35:28 -0700

    FEATURE: Add badges display

--- javascripts/discourse/api-initializers/example-component.js
+++ javascripts/discourse/api-initializers/example-component.js
@@ -1,5 +1,13 @@
-import { apiInitializer } from 'discourse/lib/api';
+import { apiInitializer } from "discourse/lib/api";
+import { ajax } from 'discourse/lib/ajax';
 
-export default apiInitializer('0.11.1', (api) => {
-  api.registerConnectorClass('below-site-header', 'display-badges', {});
+export default apiInitializer("0.11.1", api => {
+  api.registerConnectorClass('below-site-header', 'notification-banner', {
+    setupComponent(args, component) {
+      ajax('/badges.json').then((badgeData) => {
+        let badges = badgeData.badges;
+        component.set('badges', badges);
+      });
+    },
+  });
 });
